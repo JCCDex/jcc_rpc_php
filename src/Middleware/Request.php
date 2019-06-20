c2e26dccdf22966ef0e7d2eb3326f0aed5f619b9
chenxi <bbxycx.18@163.com>
2019-06-20 18:11:57 +0800

    middleware request

--- src/Middleware/Request.php
+++ src/Middleware/Request.php
@@ -5,15 +5,57 @@ namespace JccDex\Middleware;
 
 use Psr\Http\Message\RequestInterface;
 
-class Request
+class HMACRequestHandler
 {
-    public function __construct()
-    {
+    /**
+     * @var Key
+     */
+    private $key;
 
+    /**
+     * @var Secret
+     */
+    private $secret;
+
+    /**
+     * HMACRequestHandler constructor.
+     * @param $key
+     * @param $secret
+     */
+    public function __construct($key, $secret)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
     }
 
-    public function handle()
+    /**
+     * @param callable $handler
+     * @return \Closure
+     */
+    public function __invoke(callable $handler)
     {
+        return function (RequestInterface $request, array $options) use ($handler) {
+            $dateStr = Carbon::now()->format('D, d M Y H:i:s e');
+            $signature = $this->generateSignature(sprintf("date: %s", $dateStr));
+            $request = $request->withAddedHeader('Date', $dateStr);
+            $headerValue = 'Signature keyId="%s",algorithm="hmac-sha1",signature="%s"';
+            $request = $request->withAddedHeader(
+                'Authorization',
+                sprintf($headerValue, $this->key, $signature)
+            );
+            return $handler($request, $options);
+        };
+    }
 
+    /**
+     * @param $payload
+     * @return string
+     */
+    private function generateSignature($payload)
+    {
+        $signature = hash_hmac("sha1", $payload, $this->secret, true);
+        $signature = base64_encode($signature);
+        $signature = rawurlencode($signature);
+        return $signature;
     }
 }
